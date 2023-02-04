0904ed3eb0329d67d45f73ba163df9cf684cc306
Armin Hajibeygi <armin.beygi2000@gmail.com>
2023-02-04 18:57:20 +0330

    Add User model

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,16 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    GENDER_CHOICES = (
+        ('F', 'Female'),
+        ('M', 'Male'),
+    )
+
+    address = models.TextField(blank=True, null=True)
+    age = models.PositiveSmallIntegerField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    gender = models.CharField(
+        max_length=1, choices=GENDER_CHOICES, blank=True, null=True)
+    phone = models.CharField(max_length=15, blank=True, null=True)
