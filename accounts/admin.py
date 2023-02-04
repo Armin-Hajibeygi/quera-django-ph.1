251f1a9c51ffc29cc40995cfb3153ecef4c0afd0
Armin Hajibeygi <armin.beygi2000@gmail.com>
2023-02-04 19:23:33 +0330

    Create admin for user

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    list_display = ['username', 'email', 'first_name',
+                    'last_name', 'is_staff', 'is_active']
+    list_editable = ['is_staff', 'is_active']
+    list_filter = ['gender', 'is_staff', 'is_superuser', 'is_active']
+
+    fieldsets = (
+        (None, {
+            'fields': ('username', 'password')
+        }),
+        ('Personal info', {
+            'fields': ('first_name', 'last_name', 'email', 'gender', 'age', 'description')
+        }),
+        ('Contact info', {
+            'fields': ('phone', 'address')
+        }),
+        ('Permissions', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')
+        }),
+        (('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
